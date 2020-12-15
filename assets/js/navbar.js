64dfa227c271ef63e5a1836e1f4edd05f8bd4651
Junwon Park <junwon@product.ceo>
2020-12-14 20:23:16 -0800

    Changed CSS

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,12 +1,14 @@
-var prevScrollPos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-    document.getElementById("navMenu").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50vh";
-    document.getElementById("navMenu").style.top = "50vh";
+if (window.innerWidth <= 960) {
+  var prevScrollPos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+      document.getElementById("navMenu").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50vh";
+      document.getElementById("navMenu").style.top = "50vh";
+    }
+    prevScrollPos = currentScrollPos;
   }
-  prevScrollPos = currentScrollPos;
 }
